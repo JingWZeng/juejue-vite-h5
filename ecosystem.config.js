b58df84e503a63cec5416f08c2b9857ad995b100
zengxpang <zengxpang@163.com>
2022-05-18 21:16:46 +0800

    fix:修改ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,8 @@ module.exports = {
             ref: 'origin/main',
             repo: 'git@github.com:JingWZeng/juejue-vite-h5.git',
             path: '/workspace/juejue-vite-h5',
-            'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+            'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', //
+            // 'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
             env: {
                 NODE_ENV: 'production'
             }
