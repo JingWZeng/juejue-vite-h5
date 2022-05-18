b9f4022d2966b4e4b3e457317ff2834ee82dbbbd
zengxpang <zengxpang@163.com>
2022-05-18 21:35:29 +0800

    feat:dist

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
             ref: 'origin/main',
             repo: 'git@github.com:JingWZeng/juejue-vite-h5.git',
             path: '/workspace/juejue-vite-h5',
-            'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', //
+            'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', //
             // 'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
             env: {
                 NODE_ENV: 'production'
